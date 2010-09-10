2479c80727ce41a995102843a2e7dc14be36aa56
Mario Visic <mario@squaretalent.com>
2010-09-10 19:56:48 +0800

    initial commit, currently there are a few useful methods

---
+++ lib/mathutil.rb
@@ -0,0 +1,43 @@
+class MathUtil
+  
+  # Determines if a number is prime
+  def self.prime?(number)
+    num_loops = Math.sqrt(number)
+    for i in 2..num_loops
+      return false if (number % i == 0) && (number != i)
+    end
+    for i in 2..(number / num_loops)
+      return false if number % i == 0
+    end
+    true
+  end
+  
+  # Determines if a number of a palindrome
+  def self.is_palindrome?(number)
+    number.to_s.reverse == number.to_s
+  end
+  
+  # Returns the Factorial for the given number
+  def self.factorial(number)
+    return 1 if number == 0
+    (1..number).inject(:*)
+  end
+  
+  # Returns an array with all of the rotations available for the given number
+  def self.rotations(number)
+    
+    numbers_array = number.to_s.split('')
+    rotations     = [numbers_array.to_s.to_i]
+    
+    (numbers_array.length - 1).times do
+      first = numbers_array.shift
+      numbers_array.push(first)
+      rotations << numbers_array.to_s.to_i
+    end
+    
+    puts rotations.inspect
+    
+    
+  end
+  
+end
